dfcf35c98d8ef1a1a0a82272fc302fdec6980936
daisuketerashita <ekusiad.tera@icloud.com>
2021-06-19 14:37:39 +0900

    登録画面のエラー処理の実装

--- join/index.php
+++ join/index.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+
+//入力の確認
+if(!empty($_POST)){
+    //エラー項目の確認
+    if($_POST['name'] == ""){
+        $error['name'] = 'blank';
+    }
+    if($_POST['email'] == ""){
+        $error['email'] = 'blank';
+    }
+    if($_POST['password'] == ""){
+        $error['password'] = 'blank';
+    }
+    if(strlen($_POST['password']) < 4){
+        $error['password'] = 'length';
+    }
+
+    //画像のチェック
+    $fileName = $_FILES['image']['name'];
+    if(!empty($fileName)){
+        $ext = substr($fileName,-3);
+        if($ext != 'jpg' && $ext != 'gif'){
+            $error['image'] = 'type';
+        }
+    }
+    
+    //エラーがない場合
+    if(empty($error)){
+        //画像をアップロードする
+        $image = date('YmdHis').$_FILES['image']['name'];
+        move_uploaded_file($_FILES['image']['tmp_name'],'../member_picture/'.$image);
+
+        $_SESSION['join'] = $_POST;
+        $_SESSION['join']['image'] = $image;
+        header('Location: check.php');
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -17,13 +58,37 @@
             <form action="" method="post" enctype="multipart/form-data">
                 <dl>
                     <dt>ニックネーム<span class="required">必須</span></dt>
-                    <dd><input type="text" name="name" size="35" maxlength="255"></dd>
+                    <dd>
+                        <input type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['name'],ENT_QUOTES); ?>">
+                        <?php if($error['name'] == 'blank'): ?>
+                        <p class="error">*ニックネームを入力してください</p>
+                        <?php endif; ?>
+                    </dd>
                     <dt>メールアドレス<span class="required">必須</span></dt>
-                    <dd><input type="text" name="email" size="35" maxlength="255"></dd>
+                    <dd>
+                        <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'],ENT_QUOTES); ?>">
+                        <?php if($error['email'] == 'blank'): ?>
+                        <p class="error">*メールアドレスを入力してください</p>
+                        <?php endif; ?>
+                    </dd>
                     <dt>パスワード<span class="required">必須</span></dt>
-                    <dd><input type="password" name="password" size="10" maxlength="20"></dd>
+                    <dd>
+                        <input type="password" name="password" size="10" maxlength="20" value="<?php echo htmlspecialchars($_POST['password'],ENT_QUOTES); ?>">
+                        <?php if($error['password'] == 'blank'): ?>
+                        <p class="error">*パスワードを入力してください</p>
+                        <?php elseif($error['password'] == 'length'): ?>
+                        <p class="error">*パスワードは4文字以上で入力してください</p>
+                        <?php endif; ?>
+                    </dd>
                     <dt>写真など</dt>
-                    <dd><input type="file" name="image" size="35"></dd>
+                    <dd>
+                        <input type="file" name="image" size="35">
+                        <?php if($error['image'] == 'type'): ?>
+                        <p class="error">※「jpg」か「gif」の画像を指定してください</p>
+                        <?php elseif(!empty($error)): ?>
+                        <p class="error">※画像を改めて指定してください</p>
+                        <?php endif; ?>
+                    </dd>
                 </dl>
                 <div><input type="submit" value="入力内容を確認する"></div>
             </form>
